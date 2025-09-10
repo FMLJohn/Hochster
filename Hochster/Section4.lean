6b7771900e78a024cdeb2584ee3c03c47a3889d5
FMLJohn <417963616@qq.com>
2025-09-10 08:07:50 +0100

    state a new lemma

--- Hochster/Section4.lean
+++ Hochster/Section4.lean
@@ -443,4 +443,13 @@ def isIndex.closureInsertDiv {X : Type*} [TopologicalSpace X] {i : X → Type*}
     ⟨(hAv.isClosed_vanishing_set_of_forall_map_apply_le_of_forall_ne_zero ha hb hab hr h1 h2).1,
       hA.isCompact_support_of_mem_closure_insert_div ha hb hab hr⟩
 
+lemma isIndex.map_apply_le_one_of_forall_map_apply_le_of_forall_ne_zero
+    {X : Type*} [TopologicalSpace X] {i : X → Type*} [(x : X) → Field (i x)]
+    {v : Π p : σ(X), Valuation (i p.z.1) NNRat} {A : Subring (Π x : X, i x)}
+    {hA : SpringLike' A} (hAv : hA.isIndex v) {a b r : Π x : X, i x} (ha : a ∈ A) (hb : b ∈ A)
+    (hab : ∀ x : X, b x = 0 → a x = 0) (hr : r ∈ closure (A.carrier.insert (a / b)))
+    {p : σ(X)} (hrp : r p.z.1 ≠ 0) (h1 : ∀ p : σ(X), a p.z.1 ≠ 0 → v p (a p.z.1) ≤ v p (b p.z.1))
+    (h2 : ∀ p : σ(X), a p.z.1 ≠ 0 → v p (a p.z.1) = v p (b p.z.1) → a p.z.2 ≠ 0) :
+    v p (r p.z.1) ≤ 1 := sorry
+
 end SpringLike'
