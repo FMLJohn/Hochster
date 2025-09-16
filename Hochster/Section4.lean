6450b97f63c37c7871991c54fe80061240f51614
FMLJohn <417963616@qq.com>
2025-09-16 14:30:33 +0100

    further

--- Hochster/Section4.lean
+++ Hochster/Section4.lean
@@ -592,7 +592,7 @@ lemma isIndex.map_apply_eq_one_iff_apply_ne_zero_of_forall_map_apply_le_of_foral
         zero_lt_one, or_true, this]
       exact hAv.forall_iff_of_ne p ((repPoly hr).coeff 0) (coeff_repPoly_mem hr 0) hrp
 
-lemma isIndex.wef
+lemma isIndex.exists_le_map_apply_of_forall_map_apply_le_of_forall_apply_ne_zero
     {X : Type*} [TopologicalSpace X] {i : X → Type*} [(x : X) → Field (i x)]
     {v : Π p : σ(X), Valuation (i p.z.1) NNRat} {A : Subring (Π x : X, i x)}
     {hA : SpringLike' A} (hAv : hA.isIndex v) {a b r : Π x : X, i x} (ha : a ∈ A) (hb : b ∈ A)
