4a550415216fd7b06e2e0c2dc8f988efa7995d87
FMLJohn <417963616@qq.com>
2025-09-16 04:55:05 +0100

    further

--- Hochster/Section4.lean
+++ Hochster/Section4.lean
@@ -537,6 +537,11 @@ lemma isIndex.map_apply_eq_one_iff_apply_ne_zero_of_forall_map_apply_le_of_foral
       ·
         have := lt_of_le_of_ne (h1 p hap) hvpab
         have := h4 hvpab
+        have : (repPoly hr).coeff 0 p.z.2 ≠ 0 := by
+          have := repPoly_eval_eq hr ▸ hrp0
+          simpa only [Pi.polynomial_eval_apply', Pi.div_apply, h4 hvpab, zero_div, zero_pow_eq,
+            mul_ite, mul_one, mul_zero, Finset.sum_ite_eq', Finset.mem_range, add_pos_iff,
+            zero_lt_one, or_true] using this
         have : (repPoly hr).coeff 0 p.z.1 ≠ 0 := by
           intro h
           have : ((r - (repPoly hr).coeff 0) * b ^ (repPoly hr).natDegree) p.z.1 ≠ 0 :=
