5e850eb8cab94b1c6a410a43040ba15f02a95520
FMLJohn <128468682+FMLJohn@users.noreply.github.com>
2025-11-06 20:34:52 +0000

    strange

--- Hochster/Section5.lean
+++ Hochster/Section5.lean
@@ -1,4 +1,8 @@
+import Mathlib.Algebra.Ring.Hom.Defs
+
 import Hochster.Section4
+
+open Field Subring
 
 /--
 Note that this is different from the definition of a simple spring in the paper.
@@ -15,15 +19,30 @@ structure SpringLike'.isSimple {X : Type*}
 
 namespace SpringLike'.isIndex
 
-def indExtForVIsSimpleOfIsSimple {X : Type*} [TopologicalSpace X] {i : X → Type*}
-    [(x : X) → Field (i x)] {v : Π p : σ(X), Valuation (i p.z.1) NNRat}
-    {A : Subring (Π x : X, i x)} {hA : SpringLike' A} (h : hA.isSimple)
-    (hAv : hA.isIndex v) (n : ℕ) : (hAv.indExtForV n).isSimple where
-  F := sorry
-  field := sorry
-  h := sorry
-  forall_injective := sorry
-  forall_finite := sorry
+def indExtForVSuccIsSimpleOfIsSimple {X : Type*} [TopologicalSpace X]
+    {i : X → Type*} [(x : X) → Field (i x)] {v : Π p : σ(X), Valuation (i p.z.1) NNRat}
+    {A : Subring (Π x : X, i x)} {hA : SpringLike' A} {hAv : hA.isIndex v}
+    {n : ℕ} (h : (hAv.indExtForV n).isSimple) :
+    (hAv.indExtForV (n + 1)).isSimple where
+  F := h.F
+  field := h.field
+  h := h.h
+  forall_injective := h.forall_injective
+  forall_finite := fun a ha => by
+    refine closure_induction ?_ ?_ ?_ ?_ ?_ ?_ ha
+    · intro a ha
+      refine Or.elim ha (fun ha => h.forall_finite a ha) ?_
+      · intro ⟨b, c, habc, hb, hc, hcb, hAacn, hAacnv⟩
+        refine habc ▸ (@(h.forall_finite b hb).div _ h.field.toDiv _ _
+          (h.forall_finite c hc)).subset ?_
+        · intro f ⟨x, hbcfx⟩
+          refine ⟨h.h x (b x), ⟨x, rfl⟩, h.h x (c x), ⟨x, rfl⟩, ?_⟩
+          sorry
+    · sorry
+    · sorry
+    · sorry
+    · sorry
+    · sorry
 
 def iSupExtForVIsSimpleOfIsSimple {X : Type*} [TopologicalSpace X]
     {i : X → Type*} [(x : X) → Field (i x)] {v : Π p : σ(X), Valuation (i p.z.1) NNRat}
