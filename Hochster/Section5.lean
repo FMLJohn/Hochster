a1afc45ce5fb99d8a8af08b10fcc96f700015da5
FMLJohn <128468682+FMLJohn@users.noreply.github.com>
2025-11-06 17:57:24 +0000

    change a definition

--- Hochster/Section5.lean
+++ Hochster/Section5.lean
@@ -1,14 +1,17 @@
 import Hochster.Section4
 
-/-- The spring that `hA` represents is simple. -/
+/--
+Note that this is different from the definition of a simple spring in the paper.
+It is actually stronger!
+-/
 structure SpringLike'.isSimple {X : Type*}
     [TopologicalSpace X] {i : X → Type*} [(x : X) → Field (i x)]
     {A : Subring (Π x : X, i x)} (hA : SpringLike' A) where
   F : Type*
   field : Field F
-  h (x : X) : A.map (Pi.evalRingHom i x) →+* F
+  h (x : X) : i x →+* F
   forall_injective (x : X) : Function.Injective (h x)
-  forall_finite (a : A) : { h x ⟨a.1 x, a.1, a.2, rfl⟩ | x : X }.Finite
+  forall_finite : ∀ a ∈ A, { h x (a x) | x : X }.Finite
 
 namespace SpringLike'.isIndex
 
