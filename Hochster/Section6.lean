93428f2b47e43a887b78f6e998b974b838c858eb
FMLJohn <128468682+FMLJohn@users.noreply.github.com>
2025-12-02 21:43:20 +0000

    `Category SWICat`

--- Hochster/Section6.lean
+++ Hochster/Section6.lean
@@ -1,6 +1,6 @@
 import Hochster.Section5
 
-open TopologicalSpace
+open CategoryTheory Function TopologicalSpace
 
 /-- The category of spaces with indeterminates. -/
 @[ext]
@@ -15,3 +15,38 @@ structure SWICat where
   eq_generateFrom : tX = generateFrom (Set.range g)
 
 attribute [instance] SWICat.tX SWICat.spectralSpace
+
+namespace SWICat
+
+structure Hom (I J : SWICat) where
+  f : I.X → J.X
+  isSpectralMap : IsSpectralMap f
+  r : J.E → I.E
+  injective : Function.Injective r
+  comp_eq_comp : I.g ∘ r = (fun o => f ⁻¹' o) ∘ J.g
+
+def Hom.id (I : SWICat) : Hom I I where
+  f := fun x => x
+  isSpectralMap := isSpectralMap_id
+  r := fun e => e
+  injective := injective_id
+  comp_eq_comp := by ext; simp
+
+def Hom.comp {I J K : SWICat} (hom1 : I.Hom J) (hom2 : J.Hom K) : I.Hom K where
+  f := hom2.f ∘ hom1.f
+  isSpectralMap := hom2.isSpectralMap.comp hom1.isSpectralMap
+  r := hom1.r ∘ hom2.r
+  injective := hom1.injective.comp hom2.injective
+  comp_eq_comp := funext fun e => comp_apply (g := K.g) ▸ Set.preimage_comp.symm ▸
+    (comp_apply (g := K.g) ▸ funext_iff.1 hom2.comp_eq_comp e) ▸ comp_assoc .. ▸ comp_apply ▸
+    comp_apply (f := J.g) ▸ funext_iff.1 hom1.comp_eq_comp (hom2.r e)
+
+instance : Category SWICat where
+  Hom I J := Hom I J
+  id I := Hom.id I
+  comp hom1 hom2 := hom1.comp hom2
+  id_comp _ := rfl
+  comp_id _ := rfl
+  assoc _ _ _ := rfl
+
+end SWICat
