171ad37690857a04fd05e385d8864cfe96d75bf2
FMLJohn <128468682+FMLJohn@users.noreply.github.com>
2025-12-02 19:20:27 +0000

    define `SWICat`

--- Hochster/Section6.lean
+++ Hochster/Section6.lean
@@ -1,1 +1,17 @@
 import Hochster.Section5
+
+open TopologicalSpace
+
+/-- The category of spaces with indeterminates. -/
+@[ext]
+structure SWICat where
+  X : Type*
+  tX : TopologicalSpace X
+  spectralSpace : SpectralSpace X
+  E : Type*
+  g : E → Set X
+  forall_isCompact (e : E) : IsCompact (g e)
+  forall_isOpen (e : E) : IsOpen (g e)
+  eq_generateFrom : tX = generateFrom (Set.range g)
+
+attribute [instance] SWICat.tX SWICat.spectralSpace
