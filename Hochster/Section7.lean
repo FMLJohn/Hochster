0755a94dab5ec288de77bf8b9290688ad6ba18f5
FMLJohn <128468682+FMLJohn@users.noreply.github.com>
2026-01-24 18:42:29 +0000

    `SpectralSpace.exists_nonempty_homeomorph`

--- Hochster/Section7.lean
+++ Hochster/Section7.lean
@@ -1,1 +1,44 @@
 import Hochster.Section6
+
+open SpectralSpace SpringLike'.isIndex SWICat
+
+universe u
+
+namespace SpectralSpace
+
+def firstSWI (X : Type*) [TopologicalSpace X] [SpectralSpace X] :
+    SWICat where
+  X := X
+  tX := inferInstance
+  spectralSpace := inferInstance
+  E := { s : Set X | IsOpen s ∧ IsCompact s }
+  g := fun e => e.1
+  forall_isCompact e := e.2.2
+  forall_isOpen e := e.2.1
+  eq_generateFrom := by
+    simpa using SpectralSpace.toPrespectralSpace.isTopologicalBasis.eq_generateFrom
+
+def hochsterRing (X k : Type*) [TopologicalSpace X] [SpectralSpace X] [Field k] :=
+  (springLike'_mapRingHomToPiFractionRing_isIndex k (firstSWI X)).iSupExtForV.springLike.spring.A
+
+noncomputable instance (X k : Type*) [TopologicalSpace X] [SpectralSpace X] [Field k] :
+    CommRing (hochsterRing X k) := by
+  delta hochsterRing
+  infer_instance
+
+noncomputable def homeomorph (X k : Type*) [TopologicalSpace X] [SpectralSpace X] [Field k] :
+    X ≃ₜ PrimeSpectrum (hochsterRing X k) :=
+  (iSupExtForV_springLike_spring_isAffine_of_isSimple
+    (((firstSWI X).springLike' k).mapRingHomToPiFractionRingIsSimple
+      ((firstSWI X).closureRangeUnionIsSimple k))
+    ((firstSWI X).springLike'_mapRingHomToPiFractionRing_isIndex k)).homeomorph
+
+/--
+This is Hochster's Theorem: every spectral space is homeomorphic to the prime spectrum of a
+commutative ring.
+-/
+theorem exists_nonempty_homeomorph (X : Type u) [TopologicalSpace X] [SpectralSpace X] :
+    ∃ R : Type u, ∃ _ : CommRing R, Nonempty (X ≃ₜ PrimeSpectrum R) :=
+  ⟨hochsterRing X ℚ, inferInstance, ⟨homeomorph X ℚ⟩⟩
+
+end SpectralSpace
